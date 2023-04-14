0c64e5f163b77ac85d7a1693815fca9db5b156e1
Likitha00 <74499909+Likitha00@users.noreply.github.com>
2023-04-14 09:19:56 +0000

    added request model functions, request details view

--- backend/DDF/request/models.py
+++ backend/DDF/request/models.py
@@ -30,3 +30,43 @@ class FundRequest(models.Model):
     hod_review = models.CharField(max_length=3000, blank=True, null=True)
     hod_review_date = models.DateTimeField(auto_now_add=False, blank=True, null=True)
     upload = models.FileField(upload_to ='uploads/')
+
+    def set_committee_approval(self, committee_review):
+        self.committee_approval_status = 'Approved'
+        self.committee_review = committee_review
+        self.committee_review_date = timezone.now()
+        self.save()
+
+    def set_committee_disapproval(self, committee_review):
+        self.committee_approval_status = 'Disapproved'
+        self.committee_review = committee_review
+        self.committee_review_date = timezone.now()
+        self.save()
+
+    def set_hod_approval(self, hod_review):
+        self.hod_approval_status = 'Approved'
+        self.hod_review = hod_review
+        self.committee_review_date = timezone.now()
+        self.save()
+
+    def set_hod_disapproval(self, hod_review):
+        self.hod_approval_status = 'Disapproved'
+        self.hod_review = hod_review
+        self.committee_review_date = timezone.now()
+        self.save()
+
+    def get_request_details(self):
+        if self.upload != '':
+            request_dict = model_to_dict(self)
+            upload_dict = {
+                'url': request_dict['upload'].url,
+                'path': request_dict['upload'].path
+            }
+            request_dict['upload'] = upload_dict
+        else:
+            request_dict = model_to_dict(self, exclude=['upload'])
+            
+        request_dict['user'] = model_to_dict(self.user, fields=['email'])
+        request_dict['request_date'] = timezone.localtime(self.request_date).strftime('%Y-%m-%d %H:%M:%S')
+
+        return request_dict
--- backend/DDF/request/urls.py
+++ backend/DDF/request/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CreatePrivateFundRequest, CreatePublicFundRequest, CreateBudgetRequest
+from .views import CreatePrivateFundRequest, CreatePublicFundRequest, CreateBudgetRequest, RequestDetails
 
 urlpatterns = [
     path('createprivaterequest', CreatePrivateFundRequest.as_view()),
     path('createpublicrequest', CreatePublicFundRequest.as_view()),
     path('createbudgetrequest', CreateBudgetRequest.as_view()),
+    path('requestdetails', RequestDetails.as_view()),
 ]
 

87435d34629b441052f6643d2b6960046491226c
Likitha00 <CS19BTECH11008@iith.ac.in>
2023-04-27 22:50:22 +0530

    added committee model test files

--- backend/DDF/committee/urls.py
+++ backend/DDF/committee/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from .views import PendingRequests,PreviousRequests, Approval, Disapproval, AllTransactions, Balance, CreditTransactions, DebitTransactions
 
+app_name = 'committee'
+
 urlpatterns = [
-    path('pendingrequests', PendingRequests.as_view()),
-    path('previousrequests', PreviousRequests.as_view()),
-    path('approve', Approval.as_view()),
-    path('disapprove', Disapproval.as_view()),
-    path('alltransactions', AllTransactions.as_view()),
-    path('viewbalance', Balance.as_view()),
-    path('credittransactions', CreditTransactions.as_view()),
-    path('debittransactions', DebitTransactions.as_view()),
+    path('pendingrequests', PendingRequests.as_view(),name = 'pendingrequests'),
+    path('previousrequests', PreviousRequests.as_view(),name = 'previousrequests'),
+    path('approve', Approval.as_view(),name = 'approve'),
+    path('disapprove', Disapproval.as_view(),name = 'disapprove'),
+    path('alltransactions', AllTransactions.as_view(),name = 'alltransactions'),
+    path('viewbalance', Balance.as_view(),name = 'viewbalance'),
+    path('credittransactions', CreditTransactions.as_view(),name = 'credittransactions'),
+    path('debittransactions', DebitTransactions.as_view(),name = 'debittransactions'),
 ]
 
--- backend/DDF/faculty/tests/test_model.py
+++ backend/DDF/faculty/tests/test_model.py
@@ -1,5 +1,32 @@
+# from unittest.mock import patch
+from unittest.mock import patch
 from django.test import TestCase
+from authentication.models import CustomUser
+from request.models import FundRequest
+from faculty.models import FacultyUser
+from request.viewrequests.pendingrequest_strategy import PendingRequestStrategy
+
 
 class FacultyUserTest(TestCase):
     def setUp(self):
-        
+        self.faculty_user = FacultyUser.objects.create_user(email = 'test@gmail.com',password ='testpassword')
+        self.fundrequest1 = FundRequest(user=self.faculty_user, request_type='PrivateRequest', request_title='Testing', request_description='This request is created for testing', request_amount=11.00)
+        self.fundrequest2 =  FundRequest(user=self.faculty_user, request_type='PrivateRequest', request_title='Testing', request_description='This request is created for testing', request_amount=111.00, transaction_type = 'Credit')
+        self.PendingRequeststrategy = PendingRequestStrategy()
+
+    def test_faculty_user_model(self):
+        self.assertEqual(FacultyUser.objects.count(), 1)
+        self.assertEqual(CustomUser.objects.count(), 1)
+        self.assertEqual(self.faculty_user.email, 'test@gmail.com')
+        self.assertTrue(self.faculty_user.check_password('testpassword'))
+
+    @patch('request.viewrequests.previousrequest_strategy.PendingRequestStrategy()')
+    @patch('request.viewrequests.previousrequest_strategy.PendingRequestStrategy.view_request()')
+    def test_view_pending_requests(self, mock_view_requests,mock_pendingrequests):
+        mock_pendingrequests.return_value = self.PendingRequeststrategy
+        mock_view_requests.return_value = [self.fundrequest1,self.fundrequest2]
+        pending_requests = self.faculty_user.view_pending_requests()
+        self.assertEqual(pending_requests,[self.fundrequest1,self.fundrequest2])
+
+
+
